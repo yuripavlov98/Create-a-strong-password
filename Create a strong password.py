b8259e8ee589605f7b258c8e9751faaadf44de5d
Yurion <yuripavlovdex@gmail.com>
2021-11-01 14:14:33 +0300

    added password characters

---
+++ Create a strong password.py
@@ -0,0 +1,8 @@
+import random
+
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+
+chars = ''
