3bf86b3ee108e118a08bc9db5646d91f202e4d55
Yurion <yuripavlovdex@gmail.com>
2021-11-01 14:16:37 +0300

    reading entered data

--- Create a strong password.py
+++ Create a strong password.py
@@ -6,3 +6,11 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 
 chars = ''
+
+n = int(input('Введите количество паролей для генерации: '))
+length = int(input('Введите длину пароля: '))
+add_digit = input('Включить цифры? (д = да, н = нет) ').strip()
+add_lowercase = input('Включить прописные буквы? (д = да, н = нет) ').strip()
+add_uppercase = input('Включить строчные буквы? (д = да, н = нет) ').strip()
+add_punctuation = input('Включить символы, такие как !#$%&*+-=?@^_? (д = да, н = нет) ').strip()
+remove_badsymbols = input('Исключить символы il1Lo0O? (д = да, н = нет)').strip()
